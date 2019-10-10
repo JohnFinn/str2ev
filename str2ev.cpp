f0719fe3bbc50f7b01734ad9ee49825926175ef4
jouni <jouni@jouni.jouni>
2019-10-10 15:54:29 +0300

    non working fake typing

--- str2ev.cpp
+++ str2ev.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <locale>
 #include <iostream>
+#include <sstream>
 
 enum KEY_ACTION {
     Release = 0,
@@ -14,18 +15,85 @@ enum KEY_ACTION {
     Autorepeat = 2
 };
 
-int main(int argc, const char** argv)
+int keys[] = {
+    KEY_0,
+    KEY_1,
+    KEY_2,
+    KEY_3,
+    KEY_4,
+    KEY_5,
+    KEY_6,
+    KEY_7,
+    KEY_8,
+    KEY_9,
+    KEY_A,
+    KEY_B,
+    KEY_C,
+    KEY_D,
+    KEY_E,
+    KEY_F,
+};
+
+void fake_do_key(KEY_ACTION action, int key)
 {
     struct input_event ev;
-    ev.type = EV_KEY;
+
+    ev.type = EV_MSC;
+    ev.code = MSC_SCAN;
+    gettimeofday(&ev.time, 0);
+    write(1, &ev, sizeof(ev));
+
+    ev.type = action;
     ev.value = Press;
-    ev.code = KEY_U;
+    ev.code = key;
+    write(1, &ev, sizeof(ev));
+
+    ev.type = EV_SYN;
+    ev.code = SYN_REPORT;
+    ev.value = SYN_REPORT;
+    write(1, &ev, sizeof(ev));
+}
+
+void fake_type_key(int key) {
+    fake_do_key(Press, key);
+    sleep(1);
+    fake_do_key(Release, key);
+}
+
+void fake_type(wchar_t symbol)
+{
+    fake_do_key(Press, KEY_LEFTSHIFT);
+    fake_do_key(Press, KEY_LEFTCTRL);
+    fake_type_key(KEY_U);
+
+    char* bytes = reinterpret_cast<char*>(&symbol);
+    for (int i = 0; i <= sizeof(wchar_t); ++i) {
+        char current_byte = bytes[i];
+        char half1 = current_byte >> 4;
+        char half2 = current_byte & 0x0f;
+        fake_type_key(keys[half1]);
+        fake_type_key(keys[half2]);
+    }
+    fake_do_key(Release, KEY_LEFTSHIFT);
+    fake_do_key(Release, KEY_LEFTCTRL);
+}
+
+std::ostream& operator<<(std::ostream& stream, const struct input_event& ev)
+{
+    return stream << " type=" << ev.type << ev.value << " value";
+}
+
+int main(int argc, const char** argv)
+{
 
     std::locale::global(std::locale("en_US.utf8"));
+    // fake_type_key('a');
 
-    for (std::wstring current; getline(std::wcin, current);) {
-        for (auto c : current)
-            std::cout << std::hex << (int)c << ' ';
-        std::cout << '\n';
-    }
+
+
+    // for (std::wstring current; getline(std::wcin, current);) {
+    //     for (auto c : current) {
+    //         fake_type(c);
+    //     }
+    // }
 }
