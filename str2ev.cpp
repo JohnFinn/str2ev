921b2ba465d1f2b2392370ad29a973109fdfb77a
jouni <jouni@jouni.jouni>
2019-10-10 18:10:14 +0300

    made program translate input strings to proper sequence of events

--- str2ev.cpp
+++ str2ev.cpp
@@ -57,7 +57,6 @@ void fake_do_key(KEY_ACTION action, int key)
 
 void fake_type_key(int key) {
     fake_do_key(Press, key);
-    sleep(1);
     fake_do_key(Release, key);
 }
 
@@ -66,68 +65,35 @@ void fake_type(wchar_t symbol)
     fake_do_key(Press, KEY_LEFTSHIFT);
     fake_do_key(Press, KEY_LEFTCTRL);
     fake_type_key(KEY_U);
+    fake_do_key(Release, KEY_LEFTSHIFT);
+    fake_do_key(Release, KEY_LEFTCTRL);
 
+    // TODO handle endiannes
     char* bytes = reinterpret_cast<char*>(&symbol);
-    for (int i = 0; i <= sizeof(wchar_t); ++i) {
-        char current_byte = bytes[i];
+    bool start_zeros = true;
+    for (int i = 0; i < sizeof(wchar_t); ++i) {
+        char current_byte = bytes[sizeof(wchar_t) - 1 - i];
+        if (start_zeros) {
+            if (current_byte == 0)
+                continue;
+            else
+                start_zeros = false;
+        }
         char half1 = current_byte >> 4;
         char half2 = current_byte & 0x0f;
         fake_type_key(keys[half1]);
         fake_type_key(keys[half2]);
+        std::cerr << std::hex << (int)half1 << ' ' << std::hex << (int)half2 << '\n';
     }
-    fake_do_key(Release, KEY_LEFTSHIFT);
-    fake_do_key(Release, KEY_LEFTCTRL);
-}
 
-
-std::ostream& operator<<(std::ostream& stream, const struct timeval& tv)
-{
-    return stream << tv.tv_sec << "s" << tv.tv_usec << "us";
-}
-
-const char* input_event_type_to_str(__u16 type)
-{
-    switch (type)
-    {
-    case EV_SYN: return "EV_SYN";
-    case EV_KEY: return "EV_KEY";
-    case EV_REL: return "EV_REL";
-    case EV_ABS: return "EV_ABS";
-    case EV_MSC: return "EV_MSC";
-    case EV_SW:  return "EV_SW";
-    case EV_LED: return "EV_LED";
-    case EV_SND: return "EV_SND";
-    case EV_REP: return "EV_REP";
-    case EV_FF:  return "EV_FF";
-    case EV_PWR: return "EV_PWR";
-    case EV_FF_STATUS: return "EV_FF_STATUS";
-    default: return "BAD";
-    }
-}
-
-std::ostream& operator<<(std::ostream& stream, const struct input_event& ev)
-{
-    return stream
-    << " type="  << input_event_type_to_str(ev.type)
-    << " code=0x"  << std::hex << ev.code 
-    << " value=0x" << std::hex << ev.value 
-    /* << " time=" << ev.time */
-    ;
+    fake_type_key(KEY_ENTER);
 }
 
 int main(int argc, const char** argv)
 {
-
-    std::locale::global(std::locale("en_US.utf8"));
-    fake_type_key(KEY_A);
-    // struct input_event ev;
-    // while (read(0, &ev, sizeof(ev))) std::cout << ev << '\n';
-
-
-
-    // for (std::wstring current; getline(std::wcin, current);) {
-    //     for (auto c : current) {
-    //         fake_type(c);
-    //     }
-    // }
+    for (std::wstring current; getline(std::wcin, current);) {
+        for (auto c : current) {
+            fake_type(c);
+        }
+    }
 }
