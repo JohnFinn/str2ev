9c8930d8bd1389bb06fc1996ecd4fd604e004f63
jouni <jouni@jouni.jouni>
2019-10-10 17:36:41 +0300

    it worked

--- str2ev.cpp
+++ str2ev.cpp
@@ -40,11 +40,12 @@ void fake_do_key(KEY_ACTION action, int key)
 
     ev.type = EV_MSC;
     ev.code = MSC_SCAN;
+    ev.value = 0x70004;
     gettimeofday(&ev.time, 0);
     write(1, &ev, sizeof(ev));
 
-    ev.type = action;
-    ev.value = Press;
+    ev.type = EV_KEY;
+    ev.value = action;
     ev.code = key;
     write(1, &ev, sizeof(ev));
 
@@ -78,16 +79,49 @@ void fake_type(wchar_t symbol)
     fake_do_key(Release, KEY_LEFTCTRL);
 }
 
+
+std::ostream& operator<<(std::ostream& stream, const struct timeval& tv)
+{
+    return stream << tv.tv_sec << "s" << tv.tv_usec << "us";
+}
+
+const char* input_event_type_to_str(__u16 type)
+{
+    switch (type)
+    {
+    case EV_SYN: return "EV_SYN";
+    case EV_KEY: return "EV_KEY";
+    case EV_REL: return "EV_REL";
+    case EV_ABS: return "EV_ABS";
+    case EV_MSC: return "EV_MSC";
+    case EV_SW:  return "EV_SW";
+    case EV_LED: return "EV_LED";
+    case EV_SND: return "EV_SND";
+    case EV_REP: return "EV_REP";
+    case EV_FF:  return "EV_FF";
+    case EV_PWR: return "EV_PWR";
+    case EV_FF_STATUS: return "EV_FF_STATUS";
+    default: return "BAD";
+    }
+}
+
 std::ostream& operator<<(std::ostream& stream, const struct input_event& ev)
 {
-    return stream << " type=" << ev.type << ev.value << " value";
+    return stream
+    << " type="  << input_event_type_to_str(ev.type)
+    << " code=0x"  << std::hex << ev.code 
+    << " value=0x" << std::hex << ev.value 
+    /* << " time=" << ev.time */
+    ;
 }
 
 int main(int argc, const char** argv)
 {
 
     std::locale::global(std::locale("en_US.utf8"));
-    // fake_type_key('a');
+    fake_type_key(KEY_A);
+    // struct input_event ev;
+    // while (read(0, &ev, sizeof(ev))) std::cout << ev << '\n';
 
 
 
